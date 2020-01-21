cb807041523824296d703e4d5eac118c9febb216
David Rodríguez <deivid.rodriguez@riseup.net>
2020-01-21 12:42:35 +0100

    SImplify

--- test/support/reline.rb
+++ test/support/reline.rb
@@ -2,10 +2,8 @@
 
 require "reline"
 
-unless STDOUT.tty? && STDIN.tty?
-  Reline.send(:remove_const, "IOGate")
-  Reline.const_set("IOGate", Reline::GeneralIO)
-  Reline.send(:core).config.instance_variable_set(:@test_mode, true)
-  Reline.send(:core).config.reset
-  Reline.input = $stdin
-end
+Reline.send(:remove_const, "IOGate")
+Reline.const_set("IOGate", Reline::GeneralIO)
+Reline.core.config.instance_variable_set(:@test_mode, true)
+Reline.core.config.reset
+Reline.input = $stdin
